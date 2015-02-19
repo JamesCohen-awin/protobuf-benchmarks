d47cc161e9e66b364f89d5b73b7f7a9d5d21adc5
James Cohen <james.cohen@affiliatewindow.com>
2015-02-19 22:22:34 +0000

    made JSON benchmark consistent with the .proto document fields

--- json-profile.php
+++ json-profile.php
@@ -2,26 +2,9 @@
 
 $json = <<<EO_JSON
 {
-    "glossary": {
-        "title": "example glossary",
-		"GlossDiv": {
-            "title": "S",
-			"GlossList": {
-                "GlossEntry": {
-                    "ID": "SGML",
-					"SortAs": "SGML",
-					"GlossTerm": "Standard Generalized Markup Language",
-					"Acronym": "SGML",
-					"Abbrev": "ISO 8879:1986",
-					"GlossDef": {
-                        "para": "A meta-markup language, used to create markup languages such as DocBook.",
-						"GlossSeeAlso": ["GML", "XML"]
-                    },
-					"GlossSee": "markup"
-                }
-            }
-        }
-    }
+    "query": "foo",
+    "page_number": 3,
+    "result_per_page": 100
 }
 EO_JSON;
 
